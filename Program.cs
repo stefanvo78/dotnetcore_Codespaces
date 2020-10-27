e23e504bf0de2eb31fa4f6dea5c7dfd647cf4e01
Stefan Volkmer <stvolkme@microsoft.com>
2020-10-27 11:50:56 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetcore_Codespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
